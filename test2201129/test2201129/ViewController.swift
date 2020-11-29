21ec1839548f1db4ea3fe560d77bd820f550f2df
swahn <>
2020-11-29 13:42:40 +0900

    kk

---
+++ test2201129/test2201129/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2201129
+//
+//  Created by swahn on 2020/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+let abc = 123
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
